f99b2ba7fc00b17ebd3d0d8df7363caa2848dafc
ESCOBAR QUEZADA ANGEL GABRIEL <ag.escobarq@uea.edu.ec>
2026-01-17 15:08:32 -0500

    Tarea semana 6

---
+++ Tarea semana 6/Sistema de Administración de Empleados/modelos/__init__.py
@@ -0,0 +1,7 @@
+# modelos/__init__.py
+"""
+Paquete que contiene las clases del modelo (entidades del dominio)
+"""
+
+from .persona import Persona
+from .empleado import Empleado
---
+++ Tarea semana 6/Sistema de Administración de Empleados/servicios/__init__.py
@@ -0,0 +1,6 @@
+# servicios/__init__.py
+"""
+Paquete que contiene la lógica de negocio y servicios de la aplicación
+"""
+
+from .gestion_empleados import GestionEmpleados
